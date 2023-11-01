304bd1b80df649b9bf36d9ba11344069764cc45d
Mohamed Baltu <mohamed@cipher.ly>
2023-11-01 11:23:12 +0200

    updates and lib folder

---
+++ lib/codegen/index.d.ts
@@ -0,0 +1,20 @@
+import * as cg from './tscodegen';
+import ts from 'typescript';
+import { OpenAPIV3 } from 'openapi-types';
+export { cg };
+export type Opts = {
+  include?: string[];
+  exclude?: string[];
+  optimistic?: boolean;
+  unionUndefined?: boolean;
+};
+export declare function generateAst(
+  spec: OpenAPIV3.Document,
+  opts: Opts,
+  isConverted: boolean,
+): ts.SourceFile;
+export declare function printAst(ast: ts.SourceFile): string;
+export declare function generateSource(
+  spec: string,
+  opts: Opts,
+): Promise<string>;
